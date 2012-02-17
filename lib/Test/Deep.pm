0fa32a8154b20803c9a78aab011e68d5ed01042d
Ricardo Signes <rjbs@cpan.org>
2012-02-16 21:42:33 -0500

    prep the next release

--- lib/Test/Deep.pm
+++ lib/Test/Deep.pm
@@ -21,7 +21,7 @@ unless (defined $Test::Deep::NoTest::NoTest)
 
 our ($Stack, %Compared, $CompareCache, %WrapCache, $Shallow);
 
-our $VERSION = '0.108';
+our $VERSION = '0.109';
 $VERSION = eval $VERSION;
 
 require Exporter;
