e855a0b1e2c96cec155a9d0cefa8480704a80498
Ricardo Signes <rjbs@cpan.org>
2012-04-01 17:54:43 +0200

    make array_each fail, not die, on non-arrayref input

--- lib/Test/Deep/ArrayEach.pm
+++ lib/Test/Deep/ArrayEach.pm
@@ -4,6 +4,7 @@ use warnings;
 package Test::Deep::ArrayEach;
 
 use Test::Deep::Cmp;
+use Scalar::Util ();
 
 sub init
 {
@@ -19,9 +20,18 @@ sub descend
 	my $self = shift;
 	my $got = shift;
 
+  return unless Scalar::Util::reftype($got) eq 'ARRAY';
 	my $exp = [ ($self->{val}) x @$got ];
 
 	return Test::Deep::descend($got, $exp);
+}
+
+sub renderExp
+{
+  my $self = shift;
+  my $exp = shift;
+
+	return '[ ' . $self->SUPER::renderExp($self->{val}) . ', ... ]';
 }
 
 1;
