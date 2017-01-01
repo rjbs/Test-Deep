4abee08bd51f430eae1a839d219707a873416432
Ricardo Signes <rjbs@cpan.org>
2017-01-01 11:39:31 -0500

    time for a stable release

--- lib/Test/Deep.pm
+++ lib/Test/Deep.pm
@@ -21,7 +21,7 @@ unless (defined $Test::Deep::NoTest::NoTest)
 
 our ($Stack, %Compared, $CompareCache, %WrapCache, $Shallow);
 
-our $VERSION = '1.125_001';
+our $VERSION = '1.126';
 $VERSION =~ tr/_//d;
 
 require Exporter;
