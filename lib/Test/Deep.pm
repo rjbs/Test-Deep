250cd4d2f3e41147d6629817e8e7c09834ab53d5
Ricardo Signes <rjbs@cpan.org>
2017-04-17 20:09:06 -0400

    prep next trial release

--- lib/Test/Deep.pm
+++ lib/Test/Deep.pm
@@ -21,7 +21,7 @@ unless (defined $Test::Deep::NoTest::NoTest)
 
 our ($Stack, %Compared, $CompareCache, %WrapCache, $Shallow);
 
-our $VERSION = '1.126';
+our $VERSION = '1.126_001';
 $VERSION =~ tr/_//d;
 
 require Exporter;
